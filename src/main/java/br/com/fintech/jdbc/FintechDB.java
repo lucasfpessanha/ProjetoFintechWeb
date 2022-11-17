3d18517c883268ab979ee3e658a3d6517acc027e
Lucas Pessanha <lucasfpessanha@gmail.com>
2022-11-17 18:17:19 -0300

    Update FintechDB.java

--- src/main/java/br/com/fintech/jdbc/FintechDB.java
+++ src/main/java/br/com/fintech/jdbc/FintechDB.java
@@ -11,7 +11,8 @@ public class FintechDB {
 
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 
-			conexao = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "system", "system");
+			conexao = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "RM94000",
+					"221091");
 
 		} catch (Exception e) {
 			e.printStackTrace();
